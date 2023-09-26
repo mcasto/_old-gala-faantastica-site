155406274214d5dba68e56e0690de6bd638008a5
Mike Casto <castoware@gmail.com>
2023-09-26 15:10:38 -0500

    Update

--- api/methods/get-biddable-items.php
+++ api/methods/get-biddable-items.php
@@ -1,5 +1,13 @@
 <?php
 function getBiddableItems($db, $request, $util)
 {
-  $util->success($db->fetchAll("SELECT * FROM biddable_items"), true);
+  $items = $db->fetchAll("SELECT * FROM biddable_items ORDER BY sort_order");
+
+  $items = array_map(function ($item) {
+    [$item->width, $item->height] = getimagesize($_SERVER['DOCUMENT_ROOT'] . $item->file_location);
+
+    return (array) $item;
+  }, $items);
+
+  $util->success($items, true);
 }
